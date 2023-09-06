7a27d3c1e10729776c0e66f3a5efffc1987285a4
varunpats <varunpatil2502@gmail.com>
2023-09-06 15:59:06 +0530

    Handling selected year

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,11 +1,18 @@
+import { useState } from 'react';
 import ExpenseItem from './ExpenseItem';
 import Card from '../UI/Card';
 import ExpensesFilter from './ExpensesFilter';
 import './Expenses.css';
 
 function Expenses(props) {
+    const [selectedYear, setSelectedYear] = useState('2021');
+
+    const onYearChange = year => {
+        setSelectedYear(year);
+    }
+
     return <Card className="expenses">
-        <ExpensesFilter />
+        <ExpensesFilter selected={selectedYear} onYearChange={onYearChange} />
         <ExpenseItem expense={props.expenses[0]} />
         <ExpenseItem expense={props.expenses[1]} />
         <ExpenseItem expense={props.expenses[2]} />
